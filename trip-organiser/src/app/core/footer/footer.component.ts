04c5c060281bc42ac416045c75f40412a5e47e2c
Patrick Labes <patrick.labes@gmail.com>
2019-07-10 14:51:38 +1000

    Updating home page, review code tonight and then deploy

--- trip-organiser/src/app/core/footer/footer.component.ts
+++ trip-organiser/src/app/core/footer/footer.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {AuthService} from "../../auth/auth.service";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  private authListenerSubs: Subscription;
+
+  userIsAuthenticated = false;
+
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
   }
 
 }
